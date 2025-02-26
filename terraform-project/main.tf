f5e890d6563025e5b539f568601e103fd4c8a902
M. Kozlik <malkak@skyvar.co.il>
2025-02-26 15:30:10 +0200

    :wrench: Test run

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -4,12 +4,3 @@ terraform {
     prefix  = "state"
   }
 }
-
-
-module "network" {
-  source = "../modules/network"
-  vpc_name = "gantim-network"
-  subnetwork_name = "snet-gantim"
-  region = "me-west1"
-  ip_cidr_range = "10.1.0.0/24"
-}

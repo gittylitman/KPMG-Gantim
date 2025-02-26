1ef19dfa585b9e4f2732035ef291ddb43434cb5b
M. Kozlik <malkak@skyvar.co.il>
2025-02-26 15:55:18 +0200

    Test run

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -4,3 +4,21 @@ terraform {
     prefix  = "state"
   }
 }
+
+module "network" {
+  source = "../modules/network"
+  vpc_name = "gantim"
+  subnetwork_name = "snet-gantim"
+  region = var.location
+  ip_cidr_range = "10.1.0.0/24"
+}
+
+module "cloud_run" {
+  source = "../modules/cloud-run"
+  cloud_run_name = var.cloud_run_name[count.index]
+  location = var.location
+  container_image = "us-docker.pkg.dev/cloudrun/container/hello"
+  vpc_access_connector_name = var.access_connector_name[count.index]
+  subnet_name = module.network.subnet_name
+  count = length(var.cloud_run_name)
+}
---
+++ terraform-project/variables.tf
@@ -0,0 +1,13 @@
+variable location {
+    default = "me-west1"
+}
+
+variable "cloud_run_name" {
+  type = list(string)
+  default = [ "gantim-admin", "gantim-citizen" ]
+}
+
+variable "access_connector_name" {
+  type = list(string)
+  default = [ "access-connector-admin", "access-connector-citizen" ]
+}

e7bbbcb925485f6d508d1db3d27fbf0effbd7361
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 08:20:41 +0200

    Remove enable cloudresourcemanager

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -5,11 +5,6 @@ terraform {
   }
 }
 
-resource "google_project_service" "cloudresourcemanager" {
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
 module "network" {
   source = "../modules/network"
   vpc_name = "gantim"

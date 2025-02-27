00db6058511309e3904a9bd2a87c8a92d31911c3
YaffaWolkowich <yafiv@skyvar.co.il>
2025-02-27 15:29:25 +0200

    Update table variable

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -11,20 +11,36 @@ module "bigquery" {
   location = "me-west-1"
   role = "BigQuery Data Editor"
   service_account_vm_name = "try-serviceaccount-vm"
-  tables = <<EOF
-[
-  {
-    "name": "permalink",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The Permalink"
-  },
-  {
-    "name": "state",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "State where the head office is located"
-  }
-]
-EOF
+  tables = [
+    {
+      table_name = "first_table"
+      columns = [
+        {
+          name = "id"
+          type = "STRING"
+          mode = "REQUIRED"
+        },
+        {
+          name = "created_at"
+          type = "TIMESTAMP"
+          mode = "NULLABLE"
+        }
+      ]
+    },
+    {
+      table_name = "second_table"
+      columns = [
+        {
+          name = "user_id"
+          type = "STRING"
+          mode = "REQUIRED"
+        },
+        {
+          name = "score"
+          type = "INTEGER"
+          mode = "NULLABLE"
+        }
+      ]
+    }
+  ]
 }

80d9d6250ed5031a36eaad7bf9a8dd053a08609f
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 08:22:50 +0200

    Add enable cloudresourcemanager

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -5,6 +5,11 @@ terraform {
   }
 }
 
+resource "google_project_service" "cloudresourcemanager" {
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
 module "network" {
   source = "../modules/network"
   vpc_name = "gantim"
@@ -21,4 +26,5 @@ module "cloud_run" {
   vpc_access_connector_name = var.access_connector_name[count.index]
   subnet_name = module.network.subnet_name
   count = length(var.cloud_run_name)
+  depends_on = [ google_project_service.cloudresourcemanager ]
 }

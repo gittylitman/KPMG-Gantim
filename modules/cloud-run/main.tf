b25de479e8ae8f80590d0dc4b3a63c5957f1db30
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 09:31:15 +0200

    Add ingress

--- modules/cloud-run/main.tf
+++ modules/cloud-run/main.tf
@@ -6,6 +6,7 @@ resource "google_project_service" "vpcaccess" {
 resource "google_cloud_run_v2_service" "cloud_run"{
   name = var.cloud_run_name
   location = var.location
+  ingress = "INGRESS_TRAFFIC_INTERNAL_ONLY"
   deletion_protection = false
   template {
     containers {

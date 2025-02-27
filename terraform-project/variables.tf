4945fd2fa92f6c4454ef7734adcf8e206a9d9619
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 09:02:01 +0200

    Add enable cloudresourcemanager

--- terraform-project/variables.tf
+++ terraform-project/variables.tf
@@ -9,5 +9,5 @@ variable "cloud_run_name" {
 
 variable "access_connector_name" {
   type = list(string)
-  default = [ "access-connector-admin01", "access-connector-citizen01" ]
+  default = [ "access-connector-admin-1", "access-connector-citizen-1" ]
 }

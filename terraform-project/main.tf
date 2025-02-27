2030615a634680886cbe20bbd5e8f68fa539339f
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 09:40:24 +0200

    Remove call modules

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -4,23 +4,3 @@ terraform {
     prefix  = "state"
   }
 }
-
-module "network" {
-  source = "../modules/network"
-  vpc_name = "gantim"
-  subnetwork_name = "snet-gantim"
-  region = var.location
-  ip_cidr_range = "10.1.0.0/28"
-}
-
-module "cloud_run" {
-  source = "../modules/cloud-run"
-  cloud_run_name = var.cloud_run_name[count.index]
-  location = var.location
-  container_image = "us-docker.pkg.dev/cloudrun/container/hello"
-  vpc_access_connector_name = var.access_connector_name[count.index]
-  subnet_name = module.network.subnet_name
-  connector_min_instances = 2
-  connector_max_instances = 4
-  count = length(var.cloud_run_name)
-}

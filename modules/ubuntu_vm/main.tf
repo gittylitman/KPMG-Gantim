75eb8be9a6dcbc04e2202750249672ced755d8c5
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 16:40:13 +0200

    remove firewall

--- modules/ubuntu_vm/main.tf
+++ modules/ubuntu_vm/main.tf
@@ -37,14 +37,14 @@ resource "google_compute_instance" "ubuntu_vm"{
     }
 }
 
-resource "google_compute_firewall" "rules" {
-  name        = "my-firewall-rule"
-  network     = var.network_name
-  description = "Creates firewall rule targeting tagged instances"
+# resource "google_compute_firewall" "rules" {
+#   name        = "my-firewall-rule"
+#   network     = var.network_name
+#   description = "Creates firewall rule targeting tagged instances"
 
-  allow {
-    protocol  = "tcp"
-    ports     = ["22", "8080", "1000-2000"]
-  }
-  source_service_accounts = [google_service_account.vm_instance_service_account.email]
-}
+#   allow {
+#     protocol  = "tcp"
+#     ports     = ["22", "8080", "1000-2000"]
+#   }
+#   source_service_accounts = [google_service_account.vm_instance_service_account.email]
+# }

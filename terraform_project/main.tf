833def2da23b56679653f440644bf9bd6ecd0960
M. Kozlik <malkak@skyvar.co.il>
2025-02-27 10:02:02 +0200

    Add bucket service

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -4,10 +4,3 @@ terraform {
     prefix  = "state"
   }
 }
-
-module "cloud_storage" {
-  source = "../modules/cloud_storage"
-  name = var.cloud_storage_name[count.index]
-  location = var.location
-  count = length(var.cloud_storage_name)
-}
--- terraform_project/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cloud_storage_name" {
-  type = list(string)
-  default = [ "cloud-storage-admin", "cloud-storage-citizen" ]
-}
-
-variable "location" {
-  default = "me-west1"
-}

0123720624d9a992ae4f155a061e37f55fe11a0d
M. Kozlik <malkak@skyvar.co.il>
2025-03-02 08:50:26 +0200

    Remove unnecessary code

--- modules/ubuntu_vm/main.tf
+++ modules/ubuntu_vm/main.tf
@@ -36,15 +36,3 @@ resource "google_compute_instance" "ubuntu_vm"{
       scopes = ["cloud-platform"]
     }
 }
-
-# resource "google_compute_firewall" "rules" {
-#   name        = "my-firewall-rule"
-#   network     = var.network_name
-#   description = "Creates firewall rule targeting tagged instances"
-
-#   allow {
-#     protocol  = "tcp"
-#     ports     = ["22", "8080", "1000-2000"]
-#   }
-#   source_service_accounts = [google_service_account.vm_instance_service_account.email]
-# }

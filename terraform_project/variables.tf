32fed28f9ae0f21f689c85fef31fbd18922a07db
Shoshana <shoshig@skyvar.co.il>
2025-03-02 10:33:05 +0200

    Fix bigquery

--- terraform_project/variables.tf
+++ terraform_project/variables.tf
@@ -1,3 +1,48 @@
 variable "project_id" {
   type = string
 }
+
+variable location {
+    default = "me-west1"
+}
+variable "cloud_run_name" {
+  type = list(string)
+  default = [ "gantim-cloudrun", "gantim-cloudrun-citizen" ]
+}
+variable "access_connector_name" {
+  type = list(string)
+  default = [ "access-connector-admin2", "access-connector-citizen2" ]
+}
+
+variable "region" {
+  default = "me-west1"
+}
+
+variable "dataset_id"{
+  type = string
+  default = "gantim-dataset"
+}
+
+variable "role" {
+  type = string
+  default = "bigquery.dataViewer" 
+}
+
+variable "table" {
+  default = [
+    {
+      table_name = "table1"
+      columns = [
+        { name = "column1", type = "STRING", mode = "NULLABLE" },
+        { name = "column2", type = "STRING", mode = "NULLABLE" }
+      ]
+    },
+    {
+      table_name = "table2"
+      columns = [
+        { name = "columnA", type = "STRING", mode = "NULLABLE" },
+        { name = "columnB", type = "STRING", mode = "NULLABLE" }
+      ]
+    }
+  ]
+}

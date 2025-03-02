54a9c1ed1609d03ce906a0b3b57869c142e2a99f
Shoshana <shoshig@skyvar.co.il>
2025-03-02 12:28:24 +0200

    fix bigquery

--- modules/connect_cloud_run_to_bigquery/main.tf
+++
@@ -1,5 +0,0 @@
-resource "google_bigquery_dataset_iam_member" "bq_access" {
-  dataset_id = var.dataset_id
-  role       = "roles/${var.role}"
-  member     = "serviceAccount:${var.cloud_run_service_account}"
-}

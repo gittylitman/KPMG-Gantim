402a09ca1c14f5c5b6c80d68a2cb064ae9475575
M. Kozlik <malkak@skyvar.co.il>
2025-03-02 10:29:19 +0200

    :zap: Create storage and network

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -8,3 +8,18 @@ terraform {
 provider "google" {
   project = var.project_id
 }
+
+module "network" {
+  source = "../modules/network"
+  vpc_name = var.vpc_name
+  subnetwork_name = var.subnet_name
+  region = var.region
+  ip_cidr_range = var.ip_cidr_range
+}
+
+module "cloud_storage" {
+  source = "../modules/cloud_storage"
+  name = var.cloud_storage_name[count.index]
+  location = var.region
+  count = length(var.cloud_storage_name)
+}
--- terraform_project/variables.tf
+++ terraform_project/variables.tf
@@ -1,3 +1,25 @@
 variable "project_id" {
   type = string
 }
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+  default = "me-west1"
+}
+
+variable "ip_cidr_range" {
+  type = string
+  description = "ip range for subnet - /28"
+}
+
+variable "cloud_storage_name" {
+  type = list(string)
+}

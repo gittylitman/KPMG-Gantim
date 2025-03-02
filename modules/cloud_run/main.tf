f917e115c1bcc155a3e0db6da7c6851c12bfc1e5
M. Kozlik <malkak@skyvar.co.il>
2025-03-02 15:50:54 +0200

    Change name of cloud run

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -31,7 +31,7 @@ resource "google_cloud_run_v2_service" "cloud_run"{
       connector = google_vpc_access_connector.connector.id
       egress = "ALL_TRAFFIC"
     }
-    service_account = google_service_account.cloudrun_service_account.id
+    service_account = google_service_account.cloudrun_service_account.email
   }
   depends_on = [ google_bigquery_dataset_iam_member.bq_access ]
 }

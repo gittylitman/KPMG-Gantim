dc4294757030fe077f22ddcc633e364d4e449333
GitiL <GitiL@skyvar.co.il>
2025-03-02 09:34:35 +0200

    Try with proxy

--- modules/proxy-subnet/main.tf
+++ modules/proxy-subnet/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_subnetwork" "proxy_subnet" {
   name          = var.subnet_name
   region        = var.region
   ip_cidr_range = var.ip_range
-  purpose       = "REGIONAL_MANAGED_PROXY"
-  role          = "PRIVATE"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
   network       = var.network_id
 }

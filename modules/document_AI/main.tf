d679abb638411f8b6ac8a75bacdd0ff70d0173fb
YaffaWolkowich <yafiv@skyvar.co.il>
2025-03-05 12:43:47 +0200

    document

--- modules/document_AI/main.tf
+++ modules/document_AI/main.tf
@@ -2,8 +2,10 @@
 resource "google_document_ai_processor" "processor" {
   location = var.location
   display_name = var.name
-  type = "DOCUMENT_PROCESSOR"
+  type = "OCR_PROCESSOR"
 }
+# DOCUMENT_TEXT_DETECTION
+  # type = "DOCUMENT_PROCESSOR"
 
 output "processor_id" {
   value = google_document_ai_processor.processor.id

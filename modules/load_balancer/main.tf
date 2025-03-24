cf46fb19e5ed602b2ff6e8866b7c18ab38f7645c
M. Kozlik <malkak@skyvar.co.il>
2025-03-24 15:47:18 +0200

    ADd

--- modules/load_balancer/main.tf
+++ modules/load_balancer/main.tf
@@ -56,7 +56,6 @@ resource "google_compute_region_url_map" "url_map" {
 
 data "google_compute_region_ssl_certificate" "ssl_cert" {
   name        = var.certificate_name
-  region = var.region
 }
 
 data "google_compute_subnetwork" "proxy_subnet" {

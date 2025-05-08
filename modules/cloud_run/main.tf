327ca872a6ac2e10997051ecf7f38f887d9b361a
M. Kozlik <malkak@skyvar.co.il>
2025-05-08 12:59:02 +0300

    Remove port 80 from back functions

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -20,9 +20,6 @@ resource "google_cloud_run_v2_service" "cloud_run"{
 
   template {
     containers {
-      ports {
-        container_port = 80
-      }
       image = var.container_image
     }
     

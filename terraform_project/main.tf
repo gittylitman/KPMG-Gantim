8cfb3580b4fdc073d69d7a1cd8f340750c690b36
S. Goldshmid <shoshig@skyvar.co.il>
2025-05-27 15:48:09 +0300

    Change names

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -5,109 +5,109 @@ terraform {
   }
 }
 
-provider "google" {
-  project = var.project_id
-}
+# provider "google" {
+#   project = var.project_id
+# }
 
-resource "google_project_service" "cloudresourcemanager" {
-  service            = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
+# resource "google_project_service" "cloudresourcemanager" {
+#   service            = "cloudresourcemanager.googleapis.com"
+#   disable_on_destroy = false
+# }
 
-resource "google_project_service" "serviceusage" {
-  service            = "serviceusage.googleapis.com"
-  disable_on_destroy = false
-  depends_on = [ google_project_service.cloudresourcemanager ]
-}
+# resource "google_project_service" "serviceusage" {
+#   service            = "serviceusage.googleapis.com"
+#   disable_on_destroy = false
+#   depends_on = [ google_project_service.cloudresourcemanager ]
+# }
 
-data "google_project" "project" {
-  project_id = var.project_id
-}
+# data "google_project" "project" {
+#   project_id = var.project_id
+# }
 
-resource "google_project_service" "iam" {
-  project = var.project_id
-  service            = "iam.googleapis.com"
-  disable_on_destroy = false
-  depends_on = [ google_project_service.serviceusage ]
-}
+# resource "google_project_service" "iam" {
+#   project = var.project_id
+#   service            = "iam.googleapis.com"
+#   disable_on_destroy = false
+#   depends_on = [ google_project_service.serviceusage ]
+# }
 
-resource "google_project_iam_binding" "project" {
-  project = var.host_project_id
-  role = "roles/compute.networkUser"
-  members = [
-      "serviceAccount:service-${data.google_project.project.number}@serverless-robot-prod.iam.gserviceaccount.com",
-  ]
-}
+# resource "google_project_iam_binding" "project" {
+#   project = var.host_project_id
+#   role = "roles/compute.networkUser"
+#   members = [
+#       "serviceAccount:service-${data.google_project.project.number}@serverless-robot-prod.iam.gserviceaccount.com",
+#   ]
+# }
 
-module "network" {
-  source = "../modules/network"
-  host_project_id = var.host_project_id
-  vpc_name = var.vpc_name
-  subnetwork_names = var.subnet_cloud_run_names
-  region = var.region
-  depends_on = [ google_project_service.serviceusage]         
-}
+# module "network" {
+#   source = "../modules/network"
+#   host_project_id = var.host_project_id
+#   vpc_name = var.vpc_name
+#   subnetwork_names = var.subnet_cloud_run_names
+#   region = var.region
+#   depends_on = [ google_project_service.serviceusage]         
+# }
 
-module "bigquery" {
-  source = "../modules/bigquery"
-  dataset_id = "${replace(var.project_name, "-", "_")}_bgquery_${var.environment}"
-  location = var.region
-  tables = var.tables
-  depends_on = [ google_project_service.cloudresourcemanager ]
-}
+# module "bigquery" {
+#   source = "../modules/bigquery"
+#   dataset_id = "${replace(var.project_name, "-", "_")}_bgquery_${var.environment}"
+#   location = var.region
+#   tables = var.tables
+#   depends_on = [ google_project_service.cloudresourcemanager ]
+# }
 
-module "cloud_run" {
-  source = "../modules/cloud_run"
-  project_id = var.project_id
-  cloud_run_name = "${var.project_name}-${var.cloud_run_names[count.index]}-${var.environment}"
-  location = var.region
-  container_image = var.container_image[count.index]
-  network_name = module.network.network_name
-  subnet_name = module.network.subnetworks_names[0].name
-  service_account_name = "${var.environment}-sa-${var.cloud_run_names[count.index]}"
-  count = length(var.cloud_run_names)
-  depends_on = [ 
-    google_project_service.cloudresourcemanager,
-    module.bigquery,
-    google_project_service.iam
-  ]
-}
+# module "cloud_run" {
+#   source = "../modules/cloud_run"
+#   project_id = var.project_id
+#   cloud_run_name = "${var.project_name}-${var.cloud_run_names[count.index]}-${var.environment}"
+#   location = var.region
+#   container_image = var.container_image[count.index]
+#   network_name = module.network.network_name
+#   subnet_name = module.network.subnetworks_names[0].name
+#   service_account_name = "${var.environment}-sa-${var.cloud_run_names[count.index]}"
+#   count = length(var.cloud_run_names)
+#   depends_on = [ 
+#     google_project_service.cloudresourcemanager,
+#     module.bigquery,
+#     google_project_service.iam
+#   ]
+# }
 
-module "front_cloud_run" {
-  source = "../modules/front_cloud_run"
-  location = var.region
-  front_cloud_run_name =  "${var.project_name}-${var.front_cloud_run_name[count.index]}-${var.environment}"
-  front_container_image = var.front_container_image[count.index]
-  network_name = module.network.network_name
-  subnet_name = module.network.subnetworks_names[1].name
-  count = length(var.front_cloud_run_name)
-  depends_on = [ google_project_service.cloudresourcemanager ]
-}
+# module "front_cloud_run" {
+#   source = "../modules/front_cloud_run"
+#   location = var.region
+#   front_cloud_run_name =  "${var.project_name}-${var.front_cloud_run_name[count.index]}-${var.environment}"
+#   front_container_image = var.front_container_image[count.index]
+#   network_name = module.network.network_name
+#   subnet_name = module.network.subnetworks_names[1].name
+#   count = length(var.front_cloud_run_name)
+#   depends_on = [ google_project_service.cloudresourcemanager ]
+# }
 
-module "load_balancer" {
-  source = "../modules/load_balancer"
-  region = var.region
-  neg_name = ["${var.project_name}-neg-${var.neg_name[0]}-${var.environment}","${var.project_name}-neg-${var.neg_name[1]}-${var.environment}"]
-  backend_service_name =["${var.project_name}-bsrv-${var.backend_service_name[0]}-${var.environment}","${var.project_name}-bsrv-${var.backend_service_name[1]}-${var.environment}"]
-  lb_name = "${var.project_name}-ilb-${var.environment}"
-  cloud_run_name = ["${var.project_name}-${var.front_cloud_run_name[0]}-${var.environment}","${var.project_name}-${var.front_cloud_run_name[1]}-${var.environment}"]
-  network_name = module.network.network_name
-  subnet_name = module.network.subnetworks_names[1].name
-  http_proxy_name = "${var.project_name}-server-prxy-${var.environment}"
-  https_forwarding_rule_name = "${var.project_name}-server-prxy-fwrule-${var.environment}"
-  host_project_id = var.host_project_id
-  depends_on = [ 
-    google_project_service.cloudresourcemanager,
-    module.front_cloud_run
-  ]
-}
+# module "load_balancer" {
+#   source = "../modules/load_balancer"
+#   region = var.region
+#   neg_name = ["${var.project_name}-neg-${var.neg_name[0]}-${var.environment}","${var.project_name}-neg-${var.neg_name[1]}-${var.environment}"]
+#   backend_service_name =["${var.project_name}-bsrv-${var.backend_service_name[0]}-${var.environment}","${var.project_name}-bsrv-${var.backend_service_name[1]}-${var.environment}"]
+#   lb_name = "${var.project_name}-ilb-${var.environment}"
+#   cloud_run_name = ["${var.project_name}-${var.front_cloud_run_name[0]}-${var.environment}","${var.project_name}-${var.front_cloud_run_name[1]}-${var.environment}"]
+#   network_name = module.network.network_name
+#   subnet_name = module.network.subnetworks_names[1].name
+#   http_proxy_name = "${var.project_name}-server-prxy-${var.environment}"
+#   https_forwarding_rule_name = "${var.project_name}-server-prxy-fwrule-${var.environment}"
+#   host_project_id = var.host_project_id
+#   depends_on = [ 
+#     google_project_service.cloudresourcemanager,
+#     module.front_cloud_run
+#   ]
+# }
 
-module "ubuntu_vm_instance" {
-  source = "../modules/ubuntu_vm"
-  service_account_vm_name = "${var.project_name}-ubut-sa-vm-${var.environment}"
-  zone = "${var.region}-${var.zone_part}"
-  vm_name = "${var.project_name}-ubut-vm-${var.environment}"
-  network_name = module.network.network_name
-  subnet_name = module.network.subnetworks_names[1].name
-  depends_on = [ google_project_service.cloudresourcemanager ]
-}
+# module "ubuntu_vm_instance" {
+#   source = "../modules/ubuntu_vm"
+#   service_account_vm_name = "${var.project_name}-ubut-sa-vm-${var.environment}"
+#   zone = "${var.region}-${var.zone_part}"
+#   vm_name = "${var.project_name}-ubut-vm-${var.environment}"
+#   network_name = module.network.network_name
+#   subnet_name = module.network.subnetworks_names[1].name
+#   depends_on = [ google_project_service.cloudresourcemanager ]
+# }
--- terraform_project/variables.tf
+++ terraform_project/variables.tf
@@ -1,79 +1,79 @@
-variable "project_id" {
-  type = string
-}
+# variable "project_id" {
+#   type = string
+# }
 
-variable "host_project_id" {
-  type = string
-}
+# variable "host_project_id" {
+#   type = string
+# }
 
-variable "project_name" {
-  type = string
-}
+# variable "project_name" {
+#   type = string
+# }
 
-variable "environment" {
-  type = string
-}
+# variable "environment" {
+#   type = string
+# }
 
-# module network
+# # module network
 
-variable "vpc_name" {
-  type = string
-}
+# variable "vpc_name" {
+#   type = string
+# }
 
-variable "subnet_cloud_run_names" {
-  type = list(string)
-  default = [ "neg-gnt-compute-back-snet" , "neg-gnt-compute-front-snet" ]
-}
+# variable "subnet_cloud_run_names" {
+#   type = list(string)
+#   default = [ "neg-gnt-compute-back-snet" , "neg-gnt-compute-front-snet" ]
+# }
 
-variable "region" {
-  type = string
-}
+# variable "region" {
+#   type = string
+# }
 
-# module bigquery
+# # module bigquery
 
-variable "tables" {
-  type = list(object({
-    table_name = string
-    columns     = list(object({
-      name   = string
-      type   = string
-      mode   = string
-    }))
-  }))
-}
+# variable "tables" {
+#   type = list(object({
+#     table_name = string
+#     columns     = list(object({
+#       name   = string
+#       type   = string
+#       mode   = string
+#     }))
+#   }))
+# }
 
-# module cloud run
+# # module cloud run
 
-variable "cloud_run_names" {
-  type = list(string)
-}
+# variable "cloud_run_names" {
+#   type = list(string)
+# }
 
-variable "container_image" {
-  type = list(string)
-}
+# variable "container_image" {
+#   type = list(string)
+# }
 
-# module front cloud run
+# # module front cloud run
 
-variable "front_cloud_run_name" {
-  type = list(string)
-}
+# variable "front_cloud_run_name" {
+#   type = list(string)
+# }
 
-variable "front_container_image" {
-  type = list(string)
-}
+# variable "front_container_image" {
+#   type = list(string)
+# }
 
-# module load balancer
+# # module load balancer
 
-variable "neg_name" {
-  type = list(string)
-}
+# variable "neg_name" {
+#   type = list(string)
+# }
 
-variable "backend_service_name" {
-  type = list(string)
-}
+# variable "backend_service_name" {
+#   type = list(string)
+# }
 
-# module vm instance
+# # module vm instance
 
-variable "zone_part" {
-  type = string
-}
+# variable "zone_part" {
+#   type = string
+# }
